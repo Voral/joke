3e7ee6f877a5c5688ecac2409932b1c500f6c2b1
Alexander Vorobyev <voral.adm@gmail.com>
2026-01-19 15:32:15 +0300

    feat: Настройка параметров сессионной куки

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,4 +4,15 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Vasoft\Joke\Core\Application;
 use Vasoft\Joke\Core\ServiceContainer;
+
+//@todo Продумать где производить настройку и какие параметры
+session_set_cookie_params([
+    'samesite' => 'Lax',
+    'secure' => $_SERVER['HTTPS'] ?? false,
+    'httponly' => true,
+    'lifetime' => 3600 * 24 * 7,
+    'path' => '/',
+    'domain' => '',
+]);
+
 return new Application(dirname(__DIR__), '/routes/web.php', new ServiceContainer());
